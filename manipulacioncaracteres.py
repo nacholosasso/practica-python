7397c2be09458441bd1db61e291f7d46eba3d941
Ignacio Lo Sasso <nacholosasso@hotmail.com>
2026-05-07 17:18:40 -0300

    manip

---
+++ manipulacioncaracteres.py
@@ -0,0 +1,16 @@
+#Asignacion
+mensaje ="Hola"
+mensaje+=" "
+mensaje+="Ernesto"
+print(mensaje)
+
+
+#Concatenacion
+print("Concatenacion)")
+mensaje= "hola"
+espacio= " "
+nombre= "Nacho"
+print("mensaje + espacio + nombre")
+
+
+
