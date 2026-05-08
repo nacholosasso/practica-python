97144123c4385be57e13ed13ffcc06095431325a
Ignacio Lo Sasso <nacholosasso@hotmail.com>
2026-05-08 09:44:48 -0300

    tipos d

---
+++ tiposdedatos.py
@@ -0,0 +1,20 @@
+#numeros entero o largos (la te da la posibilidad e poner varios elemetnos)
+numero = 15
+print(numero, type(numero))
+
+#float o numeros con decimales
+numero_decimal = 3.14
+print(numero_decimal, type(numero_decimal)) 
+
+#numeos complejos
+numero_complejo = 2 + 3j #la j es la parte imaginaria, se puede usar i pero es mas comun usar j
+print(numero_complejo, type(numero_complejo))
+
+#cadenas
+cadena = "hola mundo"
+print(cadena, type(cadena))
+
+#boleanos true o false
+verdadero_falso = 3 == 3
+print(verdadero_falso, type(verdadero_falso)) 
+
