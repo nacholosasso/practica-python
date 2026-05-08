2779a30de1e2ad1debc4ce3c1577e68afd8a4d5a
Ignacio Lo Sasso <nacholosasso@hotmail.com>
2026-05-08 09:25:44 -0300

    comentarios

---
+++ comentarios.py
@@ -0,0 +1,6 @@
+#Pueden ser # o comillas triples (''' ''') o (""" """)
+#Los comentarios son lineas de codigo que no se ejecutan, sirven para explicar el codigo    
+#Tambien se pueden usar para desactivar lineas de codigo sin borrarlas, lo que se llama "comentar codigo"
+#Se puede usar "" en realida d no son comentarios sino que al no asignar variable lo interpreta como null
+"Hola"
+#signo de gato
--- operadores.py
+++ operadores.py
@@ -1,5 +1,39 @@
+#Operadores aritméticos
+#suma o adicion
 numero_uno = 5
 numero_dos = 4
 resultado = numero_uno + numero_dos
 resultado = str(resultado)
 print("suma: " + resultado)
+#resta o substraccion
+numero_uno = 5
+numero_dos = 4
+resultado = numero_uno - numero_dos
+resultado = str(resultado)
+#multiplicacion
+print("resta: " + resultado)
+#multiplicacion
+numero_uno = 5
+numero_dos = 4
+resultado = numero_uno * numero_dos
+print("multiplicacion: " + str(resultado))
+#exponencia o potencia
+numero_uno = 2
+exponente = 5
+resultado = numero_uno ** exponente
+print("exponenciacion: " + str(resultado))
+#division
+numero_uno = 4
+numero_dos = 2
+resultado = numero_uno / numero_dos
+print("division: " + str(resultado))
+#modulo o resto ( lo que le flata a ldivision para ser un numero entero)
+numero_uno = 30
+numero_dos = 8
+resultado = numero_uno % numero_dos
+print("modulo: " + str(resultado))
+#division entera o floor division (redondea hacia abajo el resultado de la division)
+numero_uno = 10
+numero_dos = 3
+resultado = numero_uno // numero_dos
+print("division entera: " + str(resultado))
