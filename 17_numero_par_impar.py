9d735ee85a1bc81345066dcc3c9a4d87a12673a3
Ignacio Lo Sasso <nacholosasso@hotmail.com>
2026-05-11 09:43:03 -0300

    modif

--- 17_numero_par_impar.py
+++ 17_numero_par_impar.py
@@ -5,6 +5,6 @@ print("****************************************************** \n")
 numero = int(input("Por favor introduce un número entero: "))
 
 if numero % 2 == 0:
-    print("El número ", numero, " es par.")
-elif numero % 2 == 1:
-    print("El número ", numero, " es impar.")
+    print("El número", numero, "es par.")
+else:
+    print("El número", numero, "es impar.")
